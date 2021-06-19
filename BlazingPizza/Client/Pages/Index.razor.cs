331c9b586a384e5f14129aef76dd80f8019627ea
alveiroceron <alvei@LAPTOP-AH9JTENA>
2021-06-18 22:23:16 -0500

    20200618

---
+++ BlazingPizza/Client/Pages/Index.razor.cs
@@ -0,0 +1,43 @@
+﻿using BlazingPizza.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        #region Variables
+        List<PizzaSpecial> Specials;
+        Pizza ConfiguringPizza;
+        bool ShowingConfigureDialog;
+        #endregion
+
+        #region overrides
+        protected async override Task OnInitializedAsync()
+        {
+            Specials = await HttpClient.GetFromJsonAsync<List<PizzaSpecial>>("specials");
+        }
+        #endregion
+
+        #region Metodos
+        void ShowConfigPizzaDialog(PizzaSpecial special)
+        {
+            ConfiguringPizza = new()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>()
+            };
+            ShowingConfigureDialog = true;
+        }
+
+        #endregion
+    }
+}
